6161b2ee97af271ac8551aa55ca4638b620b21e3
Gerardo Rodriguez <ger.rod34@gmail.com>
2013-06-26 17:40:00 -0700

    Adjust so that admin does not add to count.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,36 +4,42 @@ class AdminController < ApplicationController
 
   def index
 
-    @parties = Party.order("current_sign_in_at ASC")
-    @guests = Guest.all
-    @children = Guest.where(age: 1..12)
-    @adults = Guest.where(age: 13..99)
+    @parties = Party.where(is_admin: false).order("current_sign_in_at ASC")
+    @guests_count = 0
+    @children_count = 0 
+    @adults_count = 0 
 
-    @parties_reserved = Party.where(status_id: 2)
+    @parties.each do |party|
+      @guests_count += party.guests.count
+      @children_count += party.guests.where(age: 1..12).count
+      @adults_count += party.guests.where(age: 13..99).count
+    end
+
+    @parties_reserved = Party.where(status_id: 2, is_admin: false)
     @adults_reserved_total = 0
     @children_reserved_total = 0
 
     @parties_reserved.each do |party|
-      @adults_reserved_total += party.guests.where(age: 13..99, attending: true).count
-      @children_reserved_total += party.guests.where(age: 1..12, attending: true).count
+      @adults_reserved_total += party.guests.where(age: 13..99).count
+      @children_reserved_total += party.guests.where(age: 1..12).count
     end
 
-    @parties_declined = Party.where(status_id: 3)
+    @parties_declined = Party.where(status_id: 3, is_admin: false)
     @adults_declined_total = 0
     @children_declined_total = 0
 
     @parties_declined.each do |party|
-      @adults_declined_total += party.guests.where(age: 13..99, attending: true).count
-      @children_declined_total += party.guests.where(age: 1..12, attending: true).count
+      @adults_declined_total += party.guests.where(age: 13..99).count
+      @children_declined_total += party.guests.where(age: 1..12).count
     end
 
-    @parties_not_responded = Party.where(status_id: 1)
+    @parties_not_responded = Party.where(status_id: 1, is_admin: false)
     @adults_not_responded_total = 0
     @children_not_responded_total = 0
 
     @parties_not_responded.each do |party|
-      @adults_not_responded_total += party.guests.where(age: 13..99, attending: true).count
-      @children_not_responded_total += party.guests.where(age: 1..12, attending: true).count
+      @adults_not_responded_total += party.guests.where(age: 13..99).count
+      @children_not_responded_total += party.guests.where(age: 1..12).count
     end
   end
 
